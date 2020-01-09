61d7b9b376311c68e00c37943d839f764c6340e4
kopair <32586709+kopair@users.noreply.github.com>
2020-01-09 15:30:50 +0800

    登录推出功能完成
    day one

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login'
+import Home from '../components/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    path: '/login',
+    component:Login
+  },
+  {
+    path: '/home',
+    component:Home
   }
 ]
 
@@ -26,6 +24,14 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if(to.path === '/login') return next();
+  const tokenStr = window.sessionStorage.getItem('token');
+  if(!tokenStr) return next('/login');
+  next();
 })
 
 export default router
