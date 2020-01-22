7fdaf270f4c40c273d01771c0870a9a7efff360c
kopair <32586709+kopair@users.noreply.github.com>
2020-01-22 17:37:03 +0800

    商品功能完成

---
+++ src/components/goods/List.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input clearable @clear="getGoodsList" placeholder="请输入内容" v-model="queryInfo.query">
+            <el-button slot="append" icon="el-icon-search" @click="getGoodsList"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="getAddPage">添加商品</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表单 -->
+      <el-table :data="goodsList" border stripe>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column label="商品价格" prop="goods_price" width="90px"></el-table-column>
+        <el-table-column label="商品重量" prop="goods_weight" width="70px"></el-table-column>
+        <el-table-column label="创建时间" width="140px">
+          <template slot-scope="scope">{{scope.row.add_time | dateFormat}}</template>
+        </el-table-column>
+        <el-table-column label="操作" width="120px">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" circle></el-button>
+            <el-button
+              @click="removeById(scope.row.goods_id)"
+              type="danger"
+              icon="el-icon-delete"
+              circle
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        background
+      ></el-pagination>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 查询参数
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      goodsList: [],
+      total: 0
+    }
+  },
+  methods: {
+    async getGoodsList() {
+      const { data: res } = await this.$http.get('goods', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品列表失败！')
+      }
+      this.$message.success('获取商品列表成功')
+      //   console.log(res.data)
+      this.goodsList = res.data.goods
+      this.total = res.data.total
+      console.log(res.data)
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getGoodsList()
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getGoodsList()
+    },
+    async removeById(id) {
+      const removeMess = await this.$confirm('此操作将永久删除该商品s, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      if(removeMess !== 'confirm'){
+          return this.$message.info('取消了删除操作')
+      }
+      const {data:res} = await this.$http.delete('goods/'+id)
+      if(res.meta.status !== 200){
+          return this.$message.error('删除失败')
+      }
+      this.$message.success('删除成功')
+      this.getGoodsList()
+    },
+    getAddPage() {
+      this.$router.push('/goods/add')
+    }
+  },
+  created() {
+    this.getGoodsList()
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
